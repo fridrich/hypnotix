3705dd86c13d44a605f88c3616b1d79a5ad26d70
Fridrich Strba <fridrich.strba@bluewin.ch>
2026-08-07 13:57:25 +0200

    Toggle the pause_button's icon in playback_bar according to the state

--- usr/lib/hypnotix/hypnotix.py
+++ usr/lib/hypnotix/hypnotix.py
@@ -955,6 +955,7 @@ class MainWindow:
             self.favorite_button.set_active(False)
             self.favorite_button_image.set_from_icon_name("xsi-non-starred-symbolic", Gtk.IconSize.BUTTON)
             self.favorite_button.set_tooltip_text(_("Add to favorites"))
+        self.update_pause_button(False)
         self.page_is_loading = False
 
     @idle_function
@@ -1081,8 +1082,15 @@ class MainWindow:
         self.info_menu_item.set_sensitive(False)
         self.playback_bar.hide()
 
+    def update_pause_button(self, is_paused: bool):
+        icon = "media-playback-start-symbolic" if is_paused else "media-playback-pause-symbolic"
+        tooltip = _("Play") if is_paused else _("Pause")
+        self.pause_button.set_image(Gtk.Image.new_from_icon_name(icon, Gtk.IconSize.BUTTON))
+        self.pause_button.set_tooltip_text(tooltip)
+
     def on_pause_button(self, widget):
         self.mpv.pause = not self.mpv.pause
+        self.update_pause_button(self.mpv.pause)
 
     def on_show_button(self, widget):
         self.navigate_to("channels_page")
