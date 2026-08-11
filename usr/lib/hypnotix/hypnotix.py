ef8168d6cbdde029cf9a40971830da7e188bed91
Fridrich Strba <fridrich.strba@bluewin.ch>
2026-08-11 17:19:16 +0200

    When asynchronous epg loading finishes, don't bring us to landing page

--- usr/lib/hypnotix/hypnotix.py
+++ usr/lib/hypnotix/hypnotix.py
@@ -2048,6 +2048,9 @@ class MainWindow:
 
     @async_function
     def reload(self, page=None, refresh=False):
+        if page is not None:
+            self.navigate_to(page)
+
         self.favorite_data = self.manager.load_favorites()
         self.status(_("Loading providers..."))
         self.providers = []
@@ -2145,8 +2148,6 @@ class MainWindow:
 
         self.refresh_providers_page()
 
-        if page is not None:
-            self.navigate_to(page)
         self.status(None)
         self.latest_search_bar_text = None
 
